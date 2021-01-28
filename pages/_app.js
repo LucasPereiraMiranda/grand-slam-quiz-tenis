386e01594fabdf2a47c19ebeeb94f10a123b89bb
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-01-28 13:30:23 -0300

    Mudar fonte de Arial para Roboto

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
-    * {
+  * {
     box-sizing: border-box;
   }
   body {
@@ -11,7 +13,7 @@ const GlobalStyle = createGlobalStyle`
     
     display: flex;
     flex-direction: column;
-    font-family: 'Arial', sans-serif;
+    font-family: 'Roboto', sans-serif;
     
     color: ${({ theme }) => theme.colors.contrastText};
   }
@@ -23,17 +25,25 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;1,400&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
